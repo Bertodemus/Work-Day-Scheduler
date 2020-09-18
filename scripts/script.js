59b457e44f00cf14fed6b006f19f27b0785dd211
Bertodemus <roberto@robertorubet.com>
2020-09-18 15:04:36 -0400

    cleard out some test code in my JS file.

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,11 @@ var aaGlance = false;
 var hourlyPlans = new Object ();
 var log;
 
-//for testing
-// var currentHour = 10;
-// console.log(parseInt(moment().format("H")));
-
-// console.log(hourlyPlans);
 
 //App startup
 //checking for current time and determining which background to apply to the time blocks
 $("textarea").each(function() {
     var currentHour = parseInt(moment().format("H"));
-    // console.log(parseInt($(this).attr("id")));
     if (parseInt($(this).attr("id")) < currentHour) {
         $(this).removeClass("present future").addClass("past");
         this.disabled = true;
@@ -48,6 +42,7 @@ function glance() {
     }
 }
 
+
 //Clock Updating
 $("#time").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a") + " " + moment.tz.zone(timeZone).abbr(timeZoneOffset));
 
@@ -60,27 +55,13 @@ setInterval(function() {
 $(".btnSave").click(function (){
     log = "log" + $(this).val();
     var hourQaulif = "#" + $(this).val();
-
-    // var tareaValue = $("textarea"+hourQaulif).val();
-    // console.log(tareaValue);
-
-    // console.log(hourQaulif);
-    // console.log(log);
-    // hourlyPlans[log] = $("textarea"+hourQaulif).val();
-    // console.log(hourlyPlans);
-
-
     hourlyPlans[log] = $("textarea"+hourQaulif).val();
-
-
     localStorage.setItem('hourlyPlans', JSON.stringify(hourlyPlans));
 })
 
 
 //planner presentation and data management
     // Function for initializing the "plans" data
-
-
     initPlans();
 
     // EventListener for clearing the locally stored plans
@@ -89,6 +70,8 @@ $(".btnSave").click(function (){
 //     scoreModalList.innerHTML = "";
 // });
 
+
+
     // Initialization function
 function initPlans() {
     var storedPlans = JSON.parse(localStorage.getItem("hourlyPlans"));
@@ -96,7 +79,6 @@ function initPlans() {
     if (storedPlans !== null) {
       hourlyPlans = storedPlans;
     }
-    console.log(hourlyPlans)
 
     renderPlans();
 }
@@ -114,7 +96,6 @@ function initPlans() {
     }
 
     $("textarea").each(function() {
-    console.log(filterTypes(["log" + $(this).attr("id")]));
         if (filterTypes(["log" + $(this).attr("id")]) !==  null) {
             $(this).text(hourlyPlans["log" + $(this).attr("id")]);
         }
@@ -122,103 +103,3 @@ function initPlans() {
     
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// SCRAP -----------------------------------------------------------------------------------------
-
-//     if ($(this).attr("id") === "text" + item) {
-
-//     }
-// });
-
-// function initView() {
-//     if ($("textarea").attr("id") === "text" + 10) {
-//         console.log($("textarea").attr("id"));
-//     }
-//      console.log($("textarea").attr("id"));
-    // var currentHour = moment().format("H");
-    // if (currentHour < 9 || currentHour > 17 ){
-
-    // }
-// }
-
-
-// console.log($("#collapse"+9))
-// $("#collapse"+9).addClass("show");
-// $(".collapse").addClass("show");
-// var hours = [9,10,11,12,13,14,15,16,17];
-// console.log($("textarea").attr("id"));
-
-// hours.forEach(initView);
-
-
-
-
-// $(".headBtn").click(glanceEye);
-    // function () {
-    // if (!aaGlance) {
-    //     $(".collapse").addClass("show");
-    //     $(".txtAuxEye").each(function() {
-    //         $(this).removeClass("fa-eye fa-eye-slash").addClass("fa-eye");
-    //     });
-    //     aaGlance = true;
-    // } else {
-    //     $(".collapse").removeClass("show");
-    //     $(".txtAuxEye").each(function() {
-    //         $(this).removeClass("fa-eye fa-eye-slash").addClass("fa-eye-slash");
-    //     });
-    //     aaGlance = false;
-    // }
-// });
-
-    // $("textarea").each(function() {
-        // hourlyPlans.some(function(log){ return log});
-        // console.log(hourlyPlans.some(function(log){ return log}));
-
-
-
-        // var logCheck = "log" + $(this).attr("id");
-        // console.log(logCheck);
-        // if (logCheck === hourlyPlans.filter(function (log){ return log.[logCheck]}))
-
-
-
-
-
-        // $(this).text(function (plan){
-        //     hourlyPlans.filter(function (log){ return })
-        // });
-    // });
-
-    // var types = {
-    //     'integer':   'int', 
-    //     'character': 'char',
-    //     'float':     'float',
-    //     'double':    'double',
-    //     'string':    'str', 
-    //     'boolean':   'bool'
-    // };
-    
-    // function filterPlans(accepted) {
-    //     var result = {};
-    //     for (var plans in hourlyPlans)
-    //         if (accepted.indexOf(plans) > -1) 
-    //             result[plans] = types[plans];
-    //     return result;
-    // }
-    // console.log(hourlyPlans);
-    // console.log(filterPlans(['log10']));
- 
-    // var types = hourlyPlans;
